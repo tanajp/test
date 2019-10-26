497b0e532d17a04d9bf4b34a4c56f190bac226c2
{tanaka} <{simplenlen@gmail.com}>
2019-10-26 14:01:05 +0900

    first commit

--- app.py
+++ app.py
@@ -17,30 +17,21 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = "61dfbc9f442a233e35131a84efaaeea5"
-channel_access_token = "iaUI9wkTtTNBmqfwohvUJ6AOLRSnU++G4EFpz2TiI/PbguxfbKFQaVKpelnea02q09PdQfq7s4ECKAiGg8KGdFQT0jaHIcDuMkvXC244fzqg5JuW46I8N4/GKclfke49DwD7g81V/vCmgLk0J61vYgdB04t89/1O/w1cDnyilFU="
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+line_channel_secret = "61dfbc9f442a233e35131a84efaaeea5"
+line_channel_access_token = "iaUI9wkTtTNBmqfwohvUJ6AOLRSnU++G4EFpz2TiI/PbguxfbKFQaVKpelnea02q09PdQfq7s4ECKAiGg8KGdFQT0jaHIcDuMkvXC244fzqg5JuW46I8N4/GKclfke49DwD7g81V/vCmgLk0J61vYgdB04t89/1O/w1cDnyilFU="
 
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+line_bot_api = LineBotApi(line_channel_access_token)
+handler = WebhookHandler(line_channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -58,7 +49,7 @@ def message_text(event):
  
     data = urllib.parse.urlencode(data).encode("utf-8")
     with urllib.request.urlopen("https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk", data=data) as res:
-        #response = res.read().decode("utf-8")
+
         reply_json = json.loads(res.read().decode("unicode_escape"))
  
         if reply_json['status'] == 0:
