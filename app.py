66a7be7d366317b4e8245a052940b6965d75fd15
tanajp <50686226+tanajp@users.noreply.github.com>
2019-10-31 20:42:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -59,11 +59,4 @@ def message_text(event):
                 TextSendMessage(text=reply))
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port ] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run()
